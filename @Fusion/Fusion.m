9e1e749f7160a93ff982b8a2d2d25fde584a96f8
Sangwoo Moon <sangwoo.moon@colorado.edu>
2018-12-05 10:28:08 -0700

    minor change: parameters

--- @Fusion/Fusion.m
+++ @Fusion/Fusion.m
@@ -2,7 +2,8 @@ classdef Fusion < handle
     properties % ( SetAccess = public, GetAccess = public )
         
         spec    % fusion specification
-        
+
+        bFusion % binary whether the agent is a fusion center
         bSend2others % The determinent array for flags to send it to other agents
         
         hist    % History
